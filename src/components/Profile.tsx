82ef5aa064dc0cf0637fcd44f70b190bad1b16af
Jean-philippe Martel <jemartel@c2r5p6.42quebec.com>
2022-09-16 12:31:27 -0400

    fixed profile layout

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,9 +1,146 @@
+function FriendList() {
+  return (
+    <div className="flex h-1/2 flex-col">
+      <div className=" rounded-lg border bg-white p-4 shadow-sm dark:border-gray-300 dark:bg-transparent sm:p-8">
+        <div className="mb-4 flex items-center justify-between">
+          <h3 className="text-xl font-bold leading-none text-gray-900 dark:text-white">
+            Latest Customers
+          </h3>
+          <a
+            href="#"
+            className="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500"
+          >
+            View all
+          </a>
+        </div>
+        <div className="flow-root">
+          <ul
+            role="list"
+            className="divide-y divide-gray-200 dark:divide-slate-300"
+          >
+            <li className="py-3 sm:py-4">
+              <div className="flex items-center space-x-4">
+                <div className="shrink-0">
+                  <img
+                    className="h-8 w-8 rounded-full"
+                    src="https://flowbite.com/docs/images/people/profile-picture-1.jpg"
+                    alt="Neil image"
+                  />
+                </div>
+                <div className="min-w-0 flex-1">
+                  <p className="truncate text-sm font-medium text-gray-900 dark:text-white">
+                    Neil Sims
+                  </p>
+                  <p className="truncate text-sm text-gray-500 dark:text-gray-400">
+                    email@windster.com
+                  </p>
+                </div>
+                <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                  $320
+                </div>
+              </div>
+            </li>
+            <li className="py-3 sm:py-4">
+              <div className="flex items-center space-x-4">
+                <div className="shrink-0">
+                  <img
+                    className="h-8 w-8 rounded-full"
+                    src="https://flowbite.com/docs/images/people/profile-picture-3.jpg"
+                    alt="Bonnie image"
+                  />
+                </div>
+                <div className="min-w-0 flex-1">
+                  <p className="truncate text-sm font-medium text-gray-900 dark:text-white">
+                    Bonnie Green
+                  </p>
+                  <p className="truncate text-sm text-gray-500 dark:text-gray-400">
+                    email@windster.com
+                  </p>
+                </div>
+                <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                  $3467
+                </div>
+              </div>
+            </li>
+            <li className="py-3 sm:py-4">
+              <div className="flex items-center space-x-4">
+                <div className="shrink-0">
+                  <img
+                    className="h-8 w-8 rounded-full"
+                    src="https://flowbite.com/docs/images/people/profile-picture-2.jpg"
+                    alt="Michael image"
+                  />
+                </div>
+                <div className="min-w-0 flex-1">
+                  <p className="truncate text-sm font-medium text-gray-900 dark:text-white">
+                    Michael Gough
+                  </p>
+                  <p className="truncate text-sm text-gray-500 dark:text-gray-400">
+                    email@windster.com
+                  </p>
+                </div>
+                <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                  $67
+                </div>
+              </div>
+            </li>
+            <li className="py-3 sm:py-4">
+              <div className="flex items-center space-x-4">
+                <div className="shrink-0">
+                  <img
+                    className="h-8 w-8 rounded-full"
+                    src="https://flowbite.com/docs/images/people/profile-picture-4.jpg"
+                    alt="Lana image"
+                  />
+                </div>
+                <div className="min-w-0 flex-1">
+                  <p className="truncate text-sm font-medium text-gray-900 dark:text-white">
+                    Lana Byrd
+                  </p>
+                  <p className="truncate text-sm text-gray-500 dark:text-gray-400">
+                    email@windster.com
+                  </p>
+                </div>
+                <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                  $367
+                </div>
+              </div>
+            </li>
+            <li className="pt-3 pb-0 sm:pt-4">
+              <div className="flex items-center space-x-4">
+                <div className="shrink-0">
+                  <img
+                    className="h-8 w-8 rounded-full"
+                    src="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
+                    alt="Thomas image"
+                  />
+                </div>
+                <div className="min-w-0 flex-1">
+                  <p className="truncate text-sm font-medium text-gray-900 dark:text-white">
+                    Thomes Lean
+                  </p>
+                  <p className="truncate text-sm text-gray-500 dark:text-gray-400">
+                    email@windster.com
+                  </p>
+                </div>
+                <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                  $2367
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
 export default function Profile() {
   // canvas should be  replace by the actual game and the chat should be replaced by the actual chat
   return (
-    <div className=" mt-12  flex    h-screen min-h-screen     flex-col  lg:ml-4 lg:h-screen lg:flex-row    ">
-      <div className="  mt-8   ml-10  h-5/6    w-5/6 rounded-lg bg-white/50 ring-1  ring-slate-300  backdrop-blur-sm  md:ml-4   lg:m-10 lg:ml-2  lg:mr-8 lg:h-5/6  lg:w-1/3 lg:p-4 ">
-        <div className="grid-rows-1">
+    <div className=" mt-16  flex    h-screen min-h-screen     flex-col   lg:h-screen lg:flex-row    ">
+      <div className="  mx-12    mt-8  h-5/6    rounded-lg bg-white/50 ring-1 ring-slate-300  backdrop-blur-sm  md:ml-4  lg:m-10   lg:mx-8 lg:h-5/6  lg:w-1/3 lg:p-4 ">
+        <div className="mb-20 grid-rows-1">
           <img
             className="mx-auto mt-12 mb-5  h-40 w-40"
             src=" https://pic.onlinewebfonts.com/svg/img_51277.png"
@@ -11,12 +148,14 @@ export default function Profile() {
           />
           <h1 className="text-center text-2xl font-bold">Username</h1>
           <h1 className="  text-center ">Rating</h1>
+          <h1 className="  text-center ">is online </h1>
           <h1 className="  text-center ">Rating == wrong</h1>
           <h1 className="text-center ">Rating == bad </h1>
         </div>
+        <FriendList />
       </div>
 
-      <div className="   mt-8 ml-10 mb-10 flex h-5/6  w-5/6 justify-center    rounded-lg bg-white/50 ring-1 ring-slate-300  backdrop-blur-sm  md:ml-4 md:mr-0  lg:m-10  lg:ml-0 lg:mr-16  lg:h-5/6 lg:w-screen  lg:p-4 "></div>
+      <div className="   mx-12    mt-8  h-full    rounded-lg bg-white/50 ring-1 ring-slate-300  backdrop-blur-sm  md:ml-4  lg:m-10   lg:ml-2 lg:mr-8  lg:h-5/6 lg:w-5/6  lg:p-4 "></div>
     </div>
   );
 }
