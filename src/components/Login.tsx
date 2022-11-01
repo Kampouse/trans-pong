653b29511167c938a351f3b39cac7fb6129dd3e5
Gabriel Asselin <gasselin@c2r4p5.42quebec.com>
2022-11-01 09:52:55 -0400

    Added Profile Images to ReactiveCanvas

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -14,11 +14,13 @@ export default function Login({ setIsAuth }) {
 	}, [])
 
   return (
-		<div className="flex h-[90%] w-screen place-items-center justify-center" >
-			<div>
-				<h1 className="font-kaushan xl:text-[250px] lg:text-[200px] md:text-[150px] sm:text-[100px] text-[75px] text-white">trans-pong</h1>
-				<button className="font-carattere bg-pink-500 text-white xl:text-4xl lg:text-3xl md:text-2xl sm:text-xl text-lg font-bold py-2 px-4 rounded mr-2 mb-2 ml-[45%]" onClick={loginRequest}>Login</button>
+		<>
+		<div className="flex h-screen w-screen">
+			<div className="m-auto" >
+					<h1 className="font-kaushan xl:text-[250px] lg:text-[200px] md:text-[150px] sm:text-[100px] text-[75px] text-white ">trans-pong</h1>
+					<button className="font-carattere bg-pink-500 text-white xl:text-4xl lg:text-3xl md:text-2xl sm:text-xl text-lg font-bold py-2 px-4 rounded mr-2 mb-2 relative ml-[50%] translate-x-[-50%]" onClick={loginRequest}>Login</button>
 			</div>
 		</div>
+		</>
   );
 }
