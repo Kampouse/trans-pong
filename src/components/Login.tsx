6ef7b6e5ddba0a98dbb222134c1500dccd2f25a2
Jean-philippe Martel <jemartel@c2r5p6.42quebec.com>
2022-11-04 11:20:40 -0400

    added a bypass for login

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -10,14 +10,10 @@ import { useLogin } from "components/App";
   email: string;
  }
 
-
-  
- // pass a paramertr ass a function
 export default function Login( Status) {
     const navigate = useNavigate();
     const [Navi , setNavi] = useState("/");
     const [islogin, setLogin] = useAtom(useLogin)
-    // const [islogin, setLogin] = useState("login");
     const [inputs, setInputs] = useState<DataIntput>({username: "", email: ""});
 	  const buttonHandler = ( func: (input:DataIntput) => void, event: React.MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
@@ -58,6 +54,10 @@ console.log(islogin)
       return data;
     })
 }  
+ let loginOffline = () => {
+  setLogin("login");
+ }
+   
 const login = async () => {
 			window.location.href =  "https://api.intra.42.fr/oauth/authorize?client_id=0b768d33ad33083e6f78a8ac6cf1f546be68c17d7fa5bf6479233bab2905f978&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fauth%2F42login&response_type=code";
 	}
@@ -72,11 +72,9 @@ const login = async () => {
 		<div className="flex h-screen w-screen">
 			<div className="m-auto" >
 					<h1 className="font-kaushan xl:text-[250px] lg:text-[200px] md:text-[150px] sm:text-[100px] text-[75px] text-white ">trans-pong</h1>
-					<button className="font-carattere bg-pink-500 text-white xl:text-4xl lg:text-3xl md:text-2xl sm:text-xl text-lg font-bold py-2 px-4 rounded mr-2 mb-2 relative ml-[50%] translate-x-[-50%]" >Login</button>
+					<button onClick={  (event) => buttonHandler(loginOffline, event) } className="font-carattere bg-pink-500 text-white xl:text-4xl lg:text-3xl md:text-2xl sm:text-xl text-lg font-bold py-2 px-4 rounded mr-2 mb-2 relative ml-[50%] translate-x-[-50%]" >Login</button>
 					<button className=" border-0  py-2 px-2.5 bg-slate-800 text-gray-200 hover:no-underline hover:shadow-none focus:no-underline focus:shadow-none focus:outline-none focus:ring-0"
 						onClick={(event) => buttonHandler(check, event) } type="submit"  >Login </button>
-
-
 			</div>
 		</div>
 		</>
