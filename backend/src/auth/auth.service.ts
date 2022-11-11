94d93d315bcda929cb8b8db35a9302aa3817a4cc
Kampouse <jpmartel98@gmail.com>
2022-11-11 21:18:56 +0100

    adding dto to auth so at somepoint it work

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -5,8 +5,53 @@ import { UpdateAuthDto } from './dto/update-auth.dto';
 @Injectable()
 export class AuthService {
  
-// check if the user is already in the database
+  create(createUserDto: CreateAuthDto) {
+      let data = {
+          email: createUserDto.email,
+          username: createUserDto.username,
+      };
+        let output = prisma.user.create({ data: data });
+          if(output != null){
+            return output;
+          }
+          else{
+            return null
+          }
+  }
+
+  findAll() {
+        let output  = prisma.user.findMany(); 
+        return output;
+  
+  }
+  /*
+  exists(email: string, username: string) {
+    let output = prisma.user.findMany({
+        where: {
+            email: email,
+            username: username,
+        },
+        select: {
+            id: true,
+        },
+    });
+    // return true or false if the user exists 
+    return output
+  }
+   */
+  
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
 
 
 
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
 }
+
+
+
+
--- backend/src/auth/dto/create-auth.dto.ts
+++ backend/src/auth/dto/create-auth.dto.ts
@@ -1,6 +1,12 @@
 export class CreateAuthDto {
 
 
+ email: string;
+    username: string ;
+    displayName: string;
+    constructor(partial: Partial<CreateAuthDto>) {
+        Object.assign(this, partial);
+    }
 
 
 
