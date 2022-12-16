41b40d4f50a2748c2fc712c71ac5f9d122785fcd
Mashk <anthony.guay112@gmail.com>
2022-12-16 15:48:26 -0500

    Profile backend2 (#13)
    * edit profile get request for private req and public one, POST upload/photo request done, will need to add authentification validation
    
    * update
    
    * backend done except authentification validation, user connected is the hardcoded useID in profile.controler.ts

--- srcs/backend/src/app.module.ts
+++ srcs/backend/src/app.module.ts
@@ -6,11 +6,18 @@ import { AuthModule } from './auth/auth.module';
 import { AuthService } from './auth/auth.service';
 import { ProfileService } from './profile/profile.service';
 import { ProfileController } from './profile/profile.controller';
+import { MulterModule } from '@nestjs/platform-express';
 // import { AuthModule } from './auth/auth.module';
 // import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    AuthModule,
+    MulterModule.register(
+    {
+        dest: '../srcs/public'
+    })
+            ],
   controllers: [AuthController, ProfileController],
   providers: [AppService, AuthService, ProfileService],
 })
