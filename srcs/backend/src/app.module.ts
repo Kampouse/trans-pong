45a7734da0b05e7ca6d442e9207a9a848246324a
jean-philippe <jp@zelexio.com>
2022-12-17 12:44:33 -0500

    status

--- srcs/backend/src/app.module.ts
+++ srcs/backend/src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthController } from './auth/auth.controller';
-import { AuthModule } from './auth/auth.module';
-import { AuthService } from './auth/auth.service';
 import { ProfileService } from './profile/profile.service';
 import { ProfileController } from './profile/profile.controller';
 import { MulterModule } from '@nestjs/platform-express';
+import { AuthModule } from './auth/auth.module';
+import { AuthController } from './auth/auth.controller';
+import { AuthService } from './auth/auth.service';
 // import { AuthModule } from './auth/auth.module';
 // import { UsersModule } from './users/users.module';
 
